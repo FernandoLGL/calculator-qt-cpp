de2d664e828ca0d3022f871993ec534100904f13
Fernando Rosendo <franplt629@gmail.com>
2020-01-01 16:40:43 -0300

    attempt to make it display better on mobile

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 
 #include "Calculator.h"
 
-int main(int argc, char *argv[]) {
-        QApplication a(argc, argv);
-        Calculator w;
-        w.show();
-        return a.exec();
+int main(int argc, char* argv[])
+{
+    //QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication a(argc, argv);
+    Calculator w;
+    w.show();
+    return a.exec();
 }
